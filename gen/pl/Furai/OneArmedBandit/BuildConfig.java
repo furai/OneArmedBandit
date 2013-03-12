8aff72a13a53bced9a48ee039ecdbc6b778429b5
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-12 16:07:26 +0100

    Added 1st part of the exercise.

---
+++ gen/pl/Furai/OneArmedBandit/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pl.Furai.OneArmedBandit;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/pl/Furai/OneArmedBandit/Game.java
@@ -0,0 +1,173 @@
+package pl.Furai.OneArmedBandit;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Game extends Activity implements OnClickListener {
+
+	private static final String TAG = "GameAcitvity";
+
+	private MyThread mThread = null;
+	private MyHandler mHandler = new MyHandler();
+
+	private ImageView image0, image1, image2;
+	private Button btnStart;
+	private AnimationDrawable animation0, animation1, animation2;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+		image0 = (ImageView) findViewById(R.id.ImageView0);
+		image1 = (ImageView) findViewById(R.id.ImageView1);
+		image2 = (ImageView) findViewById(R.id.ImageView2);
+		btnStart = (Button) findViewById(R.id.ButtonStart);
+		image0.setBackgroundResource(R.anim.fruits1);
+		image1.setBackgroundResource(R.anim.fruits2);
+		image2.setBackgroundResource(R.anim.fruits3);
+		animation0 = (AnimationDrawable) image0.getBackground();
+		animation1 = (AnimationDrawable) image1.getBackground();
+		animation2 = (AnimationDrawable) image2.getBackground();
+		/*
+		 * spinAnimation0.start(); spinAnimation1.start();
+		 * spinAnimation2.start();
+		 */
+		image0.setOnClickListener(this);
+		image1.setOnClickListener(this);
+		image2.setOnClickListener(this);
+		btnStart.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.game, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) { //
+		case R.id.action_settings:
+			startActivity(new Intent(this, SetPreferenceActivity.class)); //
+			break;
+		}
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.ButtonStart:
+			Log.v(TAG, "Clicked the start button!");
+			if (!animation0.isRunning() && !animation1.isRunning()
+					&& !animation2.isRunning()) {
+				animation0.start();
+				animation1.start();
+				animation2.start();
+				if (mThread == null) {
+					mThread = new MyThread();
+					mThread.start();
+				}
+			}
+			break;
+		case R.id.ImageView0:
+			Log.v(TAG, "Clicked the first image!");
+			if (animation0.isRunning())
+				animation0.stop();
+			else
+				Log.v(TAG, "Clicked the first image! Cannot stop.");
+			break;
+		case R.id.ImageView1:
+			Log.v(TAG, "Clicked the second image!");
+			if (animation1.isRunning())
+				animation1.stop();
+			else
+				Log.v(TAG, "Clicked the second image! Cannot stop.");
+			break;
+		case R.id.ImageView2:
+			Log.v(TAG, "Clicked the third image!");
+			if (animation2.isRunning())
+				animation2.stop();
+			else
+				Log.v(TAG, "Clicked the third image! Cannot stop.");
+			break;
+
+		}
+
+		if (!animation0.isRunning() && !animation1.isRunning()
+				&& !animation2.isRunning() && mThread != null) {
+			mThread.interrupt();
+			mThread = null;
+		}
+
+	}
+
+	private class MyThread extends Thread {
+		private static final String TAG = "Thread";
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+			Log.v(TAG, "New thread.");
+			try {
+				Message msg = mHandler.obtainMessage();
+				Bundle bundle = new Bundle();
+				Thread.sleep(10000);
+				bundle.putInt("stop", 0);
+				msg.setData(bundle);
+				msg.sendToTarget();
+				Thread.sleep(1000);
+				msg = mHandler.obtainMessage();
+				bundle.putInt("stop", 1);
+				msg.setData(bundle);
+				msg.sendToTarget();
+				Thread.sleep(1000);
+				msg = mHandler.obtainMessage();
+				bundle.putInt("stop", 2);
+				msg.setData(bundle);
+				msg.sendToTarget();
+			} catch (InterruptedException e) {
+				Log.v(TAG, "Interrupted, stopping thread. ");
+			}
+
+		}
+
+	}
+
+	private class MyHandler extends Handler {
+		private static final String TAG = "Handler";
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			Log.v(TAG, "Handling message.");
+			switch (msg.getData().getInt("stop")) {
+			case 0:
+				image0.performClick();
+				break;
+			case 1:
+				image1.performClick();
+				break;
+			case 2:
+				image2.performClick();
+				break;
+			}
+		}
+	}
+}
