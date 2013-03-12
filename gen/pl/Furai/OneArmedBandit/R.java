04200e64d85ee6bc5d235c495b3e50b42de6a1ff
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-12 16:42:07 +0100

    Added menu with bets.

--- gen/pl/Furai/OneArmedBandit/R.java
+++ gen/pl/Furai/OneArmedBandit/R.java
@@ -13,6 +13,10 @@ public final class R {
         public static final int fruits2=0x7f040001;
         public static final int fruits3=0x7f040002;
     }
+    public static final class array {
+        public static final int betNames=0x7f080001;
+        public static final int betValue=0x7f080000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -39,23 +43,25 @@ public final class R {
         public static final int ic_launcher=0x7f02000a;
     }
     public static final class id {
-        public static final int ButtonStart=0x7f0a0005;
-        public static final int ImageView0=0x7f0a0002;
-        public static final int ImageView1=0x7f0a0003;
-        public static final int ImageView2=0x7f0a0004;
-        public static final int LinearLayout=0x7f0a0000;
-        public static final int LinearLayout1=0x7f0a0001;
-        public static final int action_settings=0x7f0a0006;
+        public static final int ButtonStart=0x7f0b0005;
+        public static final int ImageView0=0x7f0b0002;
+        public static final int ImageView1=0x7f0b0003;
+        public static final int ImageView2=0x7f0b0004;
+        public static final int LinearLayout=0x7f0b0000;
+        public static final int LinearLayout1=0x7f0b0001;
+        public static final int action_settings=0x7f0b0006;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
     }
     public static final class menu {
-        public static final int game=0x7f090000;
+        public static final int game=0x7f0a0000;
     }
     public static final class string {
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
+        public static final int betPrefSummary=0x7f070005;
+        public static final int betPrefTitle=0x7f070004;
         public static final int spin=0x7f070002;
         public static final int start=0x7f070003;
     }
@@ -81,11 +87,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f090001;
     }
     public static final class xml {
         public static final int prefs=0x7f050000;
