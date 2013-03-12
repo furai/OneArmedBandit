effd149b176cb839afbe66f8302706b23ddf7302
Lucas Grzegorczyk <lucasgrzegorczyk@gmail.com>
2013-03-12 17:34:02 +0100

    Adding various comments to the code here and there.

--- src/pl/Furai/OneArmedBandit/Game.java
+++ src/pl/Furai/OneArmedBandit/Game.java
@@ -8,7 +8,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.content.res.Resources;
 import android.graphics.drawable.AnimationDrawable;
 import android.util.Log;
 import android.view.Menu;
@@ -21,12 +20,14 @@ import android.widget.Toast;
 
 public class Game extends Activity implements OnClickListener,
 		OnSharedPreferenceChangeListener {
-
+	// Prepare some global variables to later use them in code
 	private static final String TAG = "GameAcitvity";
 
 	private MyThread mThread = null;
 	private MyHandler mHandler = new MyHandler();
 
+	// No need for initialisation here - global variables are automatically
+	// initialised
 	private ImageView image0, image1, image2;
 	private Button btnStart;
 	private AnimationDrawable animation0, animation1, animation2;
@@ -36,20 +37,32 @@ public class Game extends Activity implements OnClickListener,
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+
+		// Getting image views objects
 		image0 = (ImageView) findViewById(R.id.ImageView0);
 		image1 = (ImageView) findViewById(R.id.ImageView1);
 		image2 = (ImageView) findViewById(R.id.ImageView2);
+
+		// Getting button object
 		btnStart = (Button) findViewById(R.id.ButtonStart);
+
+		// Setting animation as background
 		image0.setBackgroundResource(R.anim.fruits1);
 		image1.setBackgroundResource(R.anim.fruits2);
 		image2.setBackgroundResource(R.anim.fruits3);
+
+		// Getting animation object
 		animation0 = (AnimationDrawable) image0.getBackground();
 		animation1 = (AnimationDrawable) image1.getBackground();
 		animation2 = (AnimationDrawable) image2.getBackground();
 		/*
+		 * Don't run animations from the OnCreate
+		 * 
 		 * spinAnimation0.start(); spinAnimation1.start();
 		 * spinAnimation2.start();
 		 */
+
+		// Setting on click listeners
 		image0.setOnClickListener(this);
 		image1.setOnClickListener(this);
 		image2.setOnClickListener(this);
@@ -186,9 +199,12 @@ public class Game extends Activity implements OnClickListener,
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
 		// TODO Auto-generated method stub
-		Toast.makeText(Game.this,
-				String.format(getResources().getString(R.string.ToastBetChosen), prefs.getString(key, "Failure")),
-				Toast.LENGTH_SHORT).show();
+		Toast.makeText(
+				Game.this,
+				String.format(
+						getResources().getString(R.string.ToastBetChosen),
+						prefs.getString(key, "Failure")), Toast.LENGTH_SHORT)
+				.show();
 
 	}
 }
